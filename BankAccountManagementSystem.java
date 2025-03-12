bfa649acd238d0b6a957ef3a062e44d42326a2bd
Ishaan <ishaan0642.be21@chitkara.edu.in>
2025-03-12 14:00:56 +0530

    first commit

---
+++ BankAccountManagementSystem.java
@@ -0,0 +1,13 @@
+class Bank{
+    Bank(){
+        System.out.println("Bank class");
+    }
+}
+
+
+public class BankAccountManagementSystem {
+    public static void main(String[] args) {
+        Bank b = new Bank();
+        System.out.println("Hello world");
+    }
+}
